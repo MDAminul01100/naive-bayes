858ddc81fc1438046cc7f22ba9aec70b81d850ec
MDAminul01100 <35560083+MDAminul01100@users.noreply.github.com>
2019-03-01 02:04:00 +0600

    Update SpamDetectionMain.java

--- SpamDetectionMain.java
+++ SpamDetectionMain.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 public class SpamDetectionMain {
 	public static void main(String[] args) throws IOException
 	{
-		TrainDataForSpamDetection obj = new TrainDataForSpamDetection("C:\\Users\\Aminul\\eclipse-workspace\\dbms\\src\\spam\\data.txt");
-		//String probabilityFile = obj.makeTarinedFile();
-		obj.doCrossValidation("C:\\Users\\Aminul\\eclipse-workspace\\dbms\\File.txt");
+		TrainDataForSpamDetection obj = new TrainDataForSpamDetection();		
+		obj.doCrossValidation();
 	}
 }
